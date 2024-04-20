f225b660f9e050b3f24837c577b8d5c8f38a9205
NicDun <nicolas.dunke@gmail.com>
2024-04-20 21:42:07 +0200

    change website description.

--- frontend/src/site.config.ts
+++ frontend/src/site.config.ts
@@ -5,9 +5,9 @@ export const siteConfig: SiteConfig = {
 	// Used as both a meta property (src/components/BaseHead.astro L:31 + L:49) & the generated satori png (src/pages/og-image/[slug].png.ts)
 	author: 'nicdun',
 	// Meta property used to construct the meta title property, found in src/components/BaseHead.astro L:11
-	title: 'astro-resume-01',
+	title: 'Shortly.me - Short my url',
 	// Meta property used as the default description meta property
-	description: 'Perfect, minimal resume theme with multiple configuration option, blog integration and custom stylings. Stand tall among competitors and launch your career into orbit with Astro.js Theme Resume. Explore the cosmos of possibilities for your professional future today!',
+	description: 'Short your way to long links and make them convenient to share',
 	// HTML lang property, found in src/layouts/Base.astro L:18
 	lang: 'en-US',
 	// Meta property, found in src/components/BaseHead.astro L:42
